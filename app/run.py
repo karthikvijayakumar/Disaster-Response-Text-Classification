39c3cbd84049312583f13dedb1dc0856382719fb
Karthik Vijayakumar <maths.karthik@gmail.com>
2019-03-24 15:50:05 +0530

    q

--- app/run.py
+++ app/run.py
@@ -10,6 +10,17 @@ import pickle
 from utils import tokenize
 import os
 
+def list_files(startpath):
+    for root, dirs, files in os.walk(startpath):
+        level = root.replace(startpath, '').count(os.sep)
+        indent = ' ' * 4 * (level)
+        print('{}{}/'.format(indent, os.path.basename(root)))
+        subindent = ' ' * 4 * (level + 1)
+        for f in files:
+            print('{}{}'.format(subindent, f))
+
+list_files('..')
+
 app = Flask(__name__)
 
 print('Current working directory')
