ef02b5053e58b7bac030660d5436e6d3859ae32c
Karthik Vijayakumar <maths.karthik@gmail.com>
2019-03-30 14:06:43 +0530

    Adding word cloud that breaks and adding more detail to README

--- app/run.py
+++ app/run.py
@@ -4,11 +4,14 @@ import pandas as pd
 
 from flask import Flask
 from flask import render_template, request, jsonify
-from plotly.graph_objs import Bar
+from plotly.graph_objs import Bar, Scatter
 from sqlalchemy import create_engine
 import pickle
 from utils import tokenize
 import os
+from collections import Counter
+import itertools
+import random
 
 app = Flask(__name__)
 
@@ -28,9 +31,26 @@ with open("../models/classifier.pkl", 'rb') as f:
 def index():
     
     # extract data needed for visuals    
+
+    #Visual 1: Genre counts
     genre_counts = df.groupby('genre').count()['message']
     genre_names = list(genre_counts.index)
-    
+
+    #Visual 2: Word cloud
+    df = df.assign(cleaned_tokens = df['message'].apply(tokenize).apply(lambda x: list(filter(lambda x: not(x.isnumeric()), x)) ))
+    all_messages_combined_words_cleaned = list( itertools.chain.from_iterable(df['cleaned_tokens']) )
+    word_count_dict = dict( Counter(all_messages_combined_words_cleaned) )
+    word_count_df = pd.DataFrame.from_dict(word_count_dict, orient = 'index', columns = ['count'])
+    word_count_df['frequency'] = word_count_df['count'].apply(lambda x: x/len( all_messages_combined_words_cleaned ))
+    word_count_df = word_count_df.sort_values('frequency', ascending=False)
+    word_count_df = word_count_df[:100]
+    min_freq = word_count_df.frequency.min()
+    max_freq = word_count_df.frequency.max()
+    word_count_df = word_count_df.assign( score = word_count_df.frequency.apply(
+        lambda x: 15 + ( ((x-min_freq)/(max_freq - min_freq))*20 )
+    ))
+    colors = [plotly.colors.DEFAULT_PLOTLY_COLORS[random.randrange(1, 10)] for i in range(100)]   
+
     # create visuals    
     graphs = [
         {
@@ -48,6 +68,29 @@ def index():
                 },
                 'xaxis': {
                     'title': "Genre"
+                }
+            }
+        },
+        {
+            'data' : [
+                Scatter(x=[random.random() for i in range(100)],
+                    y=[random.random() for i in range(100)],
+                    mode='text',
+                    text=word_count_df.index,
+                    marker={'opacity': 0.3},
+                    textfont={'size': word_count_df.score,
+                            'color': colors})
+            ],
+            'layout' : {
+                'xaxis': {
+                    'showgrid': False,
+                    'showticklabels': False,
+                    'zeroline': False
+                },
+                'yaxis': {
+                    'showgrid': False, 
+                    'showticklabels': False, 
+                    'zeroline': False
                 }
             }
         }
